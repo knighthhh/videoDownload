f68170cd85bee53809d14d1d793725a3807e6af2
hayden <775340093@qq.com>
2018-12-05 17:06:28 +0800

    fix while true

--- main.py
+++ main.py
@@ -12,19 +12,28 @@
 import multithreading
 import read
 import config
+import time
 
 def main():
-    print('程序开始运行...')
-    reader = read.ReadTxt()
-    config.ALL_ID = reader.read_all_video_id()
-    mul = multithreading.Multithreading()
+    try:
+        reader = read.ReadTxt()
+        config.ALL_ID = reader.read_all_video_id()
+        mul = multithreading.Multithreading()
 
-    toutiao_uid_list = reader.read_toutiao_uid()
-    mul.start(toutiao_uid_list,'toutiao')
+        print('开始扫描今日头条。。')
+        toutiao_uid_list = reader.read_toutiao_uid()
+        mul.start(toutiao_uid_list,'toutiao')
 
-    # yidianzixun_uid_list = reader.read_yidianzixun_uid()
-    # mul.start(yidianzixun_uid_list,'yidianzixun')
+        print('开始扫描一点资讯。。')
+        yidianzixun_uid_list = reader.read_yidianzixun_uid()
+        mul.start(yidianzixun_uid_list,'yidianzixun')
+    except:
+        print('未知错误')
 
 
 if __name__ == '__main__':
-    main()
+    while True:
+        print('程序开始运行...')
+        print('当前时间：'+time.strftime('%Y-%m-%d %H:%M:%S',time.localtime()))
+        main()
+        time.sleep(60*10)
